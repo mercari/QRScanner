046ae9ce4904e28e2dd188336ba8537bf4bbf17e
Thorsten <thorsten@looking4cache.com>
2020-11-22 12:36:10 +0100

    focusView adaptive to landscape mode

--- QRScanner/QRScannerView.swift
+++ QRScanner/QRScannerView.swift
@@ -249,8 +249,8 @@ public class QRScannerView: UIView {
     }
 
     private func setupImageViews() {
-        let width = self.bounds.width * 0.618
-        let x = self.bounds.width * 0.191
+        let width = min(self.bounds.width, self.bounds.height) * 0.618
+        let x = (self.bounds.width > self.bounds.height) ? (self.bounds.width - width) / 2 : self.bounds.width * 0.191
         let y = self.bounds.height * 0.191
         focusImageView = UIImageView(frame: CGRect(x: x, y: y, width: width, height: width))
         focusImageView.image = focusImage ?? UIImage(named: "scan_qr_focus", in: Bundle(for: QRScannerView.self), compatibleWith: nil)
