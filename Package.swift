9c81351f4cb90742ab89dd8e685fde4e89dc0718
woxtu <woxtup@gmail.com>
2020-11-30 19:39:35 +0900

    Add SPM support (#24)
    * Add the package manifest
    
    * Load resources in the module
    
    * Remove redundant settings

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "QRScanner",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "QRScanner",
+            targets: ["QRScanner"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "QRScanner",
+            dependencies: [],
+            path: "QRScanner",
+            exclude: [
+                "Info.plist",
+            ]
+        ),
+    ]
+)
